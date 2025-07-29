f34e274ce5b50f81a9d6263a7452a8511e39c4b1
Addi071 <157271684+Addi071@users.noreply.github.com>
2025-07-29 14:44:35 +0530

    Update the routers

--- Backend/Routes/user.Route.js
+++ Backend/Routes/user.Route.js
@@ -1,6 +1,6 @@
 // backend/routes/user.route.js
 import express from 'express';
-import { createUser, getAllUsers, signup, login } from '../controllers/user.controller.js';
+import { createUser, getAllUsers, signup, login } from '../Controllers/user.Controller.js';
 
 let userRoute = express.Router();
 
